e3829e6178961170f6ad1fb74e87bfd3d42cf328
AfsarTanvir <afsartanvir805@gmail.com>
2025-01-06 20:59:24 +0600

    C#_Codes

---
+++ 04_Non-Primitive Types/Math/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Non_Primitive_Types.Math
+{
+    public class Calculator
+    {
+        public int Add(int x, int y) { return x + y; }
+        public int Subtract(int x, int y) {return x - y; }
+        public int Multiply(int x, int y) { return x*y; }
+        public double Divide(int x, int y) { if(y==0) return 0.0; return (double) x/y; }
+    }
+}
