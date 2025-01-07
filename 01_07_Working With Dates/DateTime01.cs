f327f162b79cc239c5dd2eb4a9d0e6378b413674
AfsarTanvir <afsartanvir805@gmail.com>
2025-01-08 01:53:24 +0600

    C# Fundamental Completed

---
+++ 01_07_Working With Dates/DateTime01.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_07_Working_With_Dates
+{
+    internal class DateTime01
+    {
+        public static void run()
+        {
+            var dateTime = new DateTime(2015, 1, 1);
+            var now = DateTime.Now;
+            var today = DateTime.Today;
+
+            Console.WriteLine(dateTime.ToString());
+            Console.WriteLine(now.ToString());
+            Console.WriteLine(today.ToString());
+            Console.WriteLine("Hour: " + now.Hour);
+            Console.WriteLine("Minute: " + now.Minute);
+
+            var tomorrow = now.AddDays(1);
+            var yesterday = now.AddDays(-1);
+            Console.WriteLine(tomorrow.ToString());
+            Console.WriteLine(yesterday.ToString());
+
+            Console.WriteLine(now.ToLongDateString());
+            Console.WriteLine(now.ToShortDateString());
+            Console.WriteLine(now.ToLongTimeString());
+            Console.WriteLine(now.ToShortTimeString());
+
+            Console.WriteLine(now.ToString("yyyy-MM-dd"));
+        }
+    }
+}
---
+++ 03_Primitive Types and Expressions/Class3.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Primitive_Types_and_Expressions
+{
+    
+    internal class Class3
+    {
+        
+    }
+}
